0e7b51eced51c82a0f043530a29ce5ce59137862
ethan berk <48163588+EthanBerk@users.noreply.github.com>
2024-11-07 14:41:18 -0700

    add test

--- ridge.py
+++ ridge.py
@@ -2,14 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from numpy.linalg import inv 
+from numpy.polynomial import *
 
 def main() :
     x = np.array([1,2,3])
     y = np.array([2,4,5])
 
-    print(GenM(x, 2))
-    print(DLS(x, y, 2))
-    print(RidgeRegression(x, y, 2, 2))
+    p1 = DLS(x, y, 4)
+    
+    
+    xeval, yeval  = p1.linspace(1000, [0,3])
+
+    print(p1)
+    
+    plt.plot(x,y, "o")
+    plt.plot(xeval,yeval)
+    plt.show()
+    
+    
     
     
     
@@ -17,13 +27,15 @@ def main() :
 def DLS(x, y, n):
     M = GenM(x,n)
     MT = np.transpose(M)
-    return inv(MT @ M) @ MT @ y
+    c = inv(MT @ M) @ MT @ y
+    return Polynomial(c)
 
 def RidgeRegression(x, y, l, n):
     M = GenM(x,n)
     I = np.identity(n)
     MT = np.transpose(M)
-    return inv(MT @ M - l * I) @ MT @ y
+    c = inv(MT @ M - l * I) @ MT @ y
+    return Polynomial(c)
     
     
 def GenM(x, n):
